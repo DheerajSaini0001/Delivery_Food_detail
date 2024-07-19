955c356cd70adc184f845e3a97d909eba4af4572
unknown <dheerajsaini131652@gmail.com>
2024-07-19 19:28:05 +0530

    go

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Detail from './Component/Detail'
+
+function App() {
+  
+  return (
+    <>
+      <Detail/>
+    </>
+  )
+}
+
+export default App
